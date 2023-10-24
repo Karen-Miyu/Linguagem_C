d46311a53576ecad70cddece9f1e4fe16fc64dd1
Karen-Miyu <126706508+Karen-Miyu@users.noreply.github.com>
2023-10-25 03:26:11 +0900

    Update Luta de Street Fighter.c

--- Códigos_em_C/Luta de Street Fighter.c
+++ Códigos_em_C/Luta de Street Fighter.c
@@ -1,13 +1,13 @@
-// O programa serve para simular o resultado de uma luta de Street Fighter
-// Os personagens dessa luta s�o Ryu e Ken
+// O programa serve para simular o resultado de uma luta de Street Fighter (que é uma série de videogame)
+// Os personagens dessa luta são Ryu e Ken
 #include <stdio.h>
 
-// Fun��o verificadora de n�mero perfeito
+// Função verificadora de número perfeito
 int NPerfeito(int n)
 {
     int SomaDivisores=0;
 
-    for(int i=1; i<=n/2;i++){ //Somar� todos os divis�veis de n
+    for(int i=1; i<=n/2;i++){ //Somará todos os divisíveis de n
         if (n%i==0){
             SomaDivisores+=i;
         }
@@ -15,12 +15,12 @@ int NPerfeito(int n)
     return SomaDivisores==n;
 }
 
-// Fun��o verificadora de n�mero triangular
+// Função verificadora de número triangular
 int NTriangular(int n)
 {
     int soma=0;
 
-    for(int i=1; soma<n; i++){  // Soma uma sequ�ncia de n�meros at� resultar em n
+    for(int i=1; soma<n; i++){  // Soma uma sequência de números até resultar em n
         soma+=i;
     }
     return soma==n;
@@ -35,15 +35,15 @@ int main()
     int Ryu=0, Ken=0;
     while(n!=0){    // Se for igual a zero nem entra no loop
         scanf("%d", &n);    // Os valores digitados devem ser inteiros positivos e negativos
-        if(n>0){    // n positivos s�o golpes de Ryu
-            if (NPerfeito(n)) { // NPerfeito retorna a fun��o, em caso verdadeiro ir� ser executado a linha de baixo
+        if(n>0){    // n positivos são golpes de Ryu
+            if (NPerfeito(n)) { // NPerfeito retorna a função, em caso verdadeiro irá ser executado a linha de baixo
                 Ryu+=(n*3);
             }else if(NTriangular(n)) {  // O mesmo vale para NTriangular
                 Ryu+=(n*2);
             }else{
                 Ryu+=n;
             }
-        }else if(n<0){ // n negativos s�o de Ken
+        }else if(n<0){ // n negativos são de Ken
             n*=-1;
             if (NPerfeito(n))  {
                 Ken+=(n*3);
@@ -54,7 +54,7 @@ int main()
             }
         }
     }
-    // Aqui � verificado o vencedor a partir dos dados obtidos anteriormente
+    // Aqui é verificado o vencedor a partir dos dados obtidos anteriormente
     if(Ken>Ryu){
         printf("\nKen venceu!\n");
     }else if(Ryu>Ken){
